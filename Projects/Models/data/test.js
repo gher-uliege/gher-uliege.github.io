d0468fad7e1c254ba29cedb20d4102d86a2d9fd8
ctroupin <charles.troupin@gmail.com>
2020-02-11 14:15:59 +0100

    model plots

---
+++ Projects/Models/data/test.js
@@ -0,0 +1,10 @@
+var models = [
+{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[0.0, 39.5], [16.0, 39.5], [16.0, 45.0], [0.0, 45.0], [0.0, 39.5]]]}, "properties": {"name": "LaMMA_NWMED"}},
+{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[10.0, 30.0], [42.0, 30.0], [42.0, 47.0], [10.0, 47.0], [10.0, 30.0]]]}, "properties": {"name": "MITO"}},
+{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[7.51, 42.38], [10.53, 42.38], [10.53, 44.42], [7.51, 44.42], [7.51, 42.38]]]}, "properties": {"name": "New Ligurian Sea"}},
+{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-18.125, 30.1875], [36.25, 30.1875], [36.25, 45.9375], [-18.125, 45.9375], [-18.125, 30.1875]]]}, "properties": {"name": "MFS_EAS1"}},
+{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[19.5, 30.4], [30.0, 30.4], [30.0, 41.0], [19.5, 41.0], [19.5, 30.4]]]}, "properties": {"name": "POSEIDON FORECASTING SYSTEM"}},
+{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[19.5, 30.4], [30.0, 30.4], [30.0, 41.0], [19.5, 41.0], [19.5, 30.4]]]}, "properties": {"name": "POSEIDON WAVE FORECASTING SYSTEM"}},
+{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-13.0, 37.0], [-0.5, 37.0], [-0.5, 45.0], [-13.0, 45.0], [-13.0, 37.0]]]}, "properties": {"name": "ROMS Iberia"}},
+{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-15.0, 35.0], [-6.0, 35.0], [-6.0, 45.0], [-15.0, 45.0], [-15.0, 35.0]]]}, "properties": {"name": "CGEOMETOC-OCEAN"}},
+];
---
+++ Projects/Models/js/basemaps.js
@@ -0,0 +1,63 @@
+
+
+var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+var CartoDB = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+});
+
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+var CartoDB_DarkMatter = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+
+var Stamen_Terrain = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+});
+
+var Esri_OceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+	maxZoom: 13
+});
+
+var Esri_DarkGreyCanvas = L.tileLayer(
+	"http://{s}.sm.mapstack.stamen.com/" +
+	"(toner-lite,$fff[difference],$fff[@23],$fff[hsl-saturation@20])/" +
+	"{z}/{x}/{y}.png",
+	{
+		attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, ' +
+		'NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+	}
+);
+
+var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+	{
+		attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, ' +
+		'AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+	}
+);
+
+var CartoDB_PositronNoLabels = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
